60129db688e4c14f9493bf73002ca4d2cdf7ed77
lixk <915547938#qq.com>
2020-03-26 17:53:49 +0800

    测试冲突

--- src/components/Photo/PhotoList.vue
+++ src/components/Photo/PhotoList.vue
@@ -31,7 +31,7 @@
         name: "PhotoList",
         data(){
             return{
-                imgList:[],
+                imgLisf:[],
                 categoryList:[]
             }
         },
